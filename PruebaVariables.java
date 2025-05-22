480aaa832637d8f51d8e3e23acebc5d1a98c6359
SantiagoGG69 <jervaciosanchez0@gmail.com>
2025-05-22 20:10:59 -0300

    Add files via upload

---
+++ PruebaVariables.java
@@ -0,0 +1,35 @@
+package trabajo;
+public class PruebaVariables { 
+
+    public static void main (String[] args){ 
+
+        int edad = 17;
+
+        System.out.println("la edad es: " + edad);
+
+        double precioproducto = 45.99;
+         boolean tienedescuento = true;
+         System.out.println("¿Tiene descuento?" + tienedescuento);
+         char letra = 'S';
+         System.out.println("La letra es" + letra);
+        int puntos = 10;
+        System.out.println("Puntos " + puntos);
+        puntos = 20;
+        System.out.println("Puntos " + puntos);
+        double notaFinal = 8;
+        System.out.println( notaFinal >=6 ? "Aprobado : Reprobado");
+        Double temperatura ;
+        temperatura = 36.5;
+        System.out.println("La temperatura es: " + temperatura);
+        int 8id = 12;
+
+        int edad1 = 18;
+        boolean Mayoredad = true;
+        System.out.println(Mayoredad ? edad1 >= 18 ? "Es mayor de edad : Es menor de edad");
+
+
+
+
+
+    }
+}
